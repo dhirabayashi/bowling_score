3aeb88f1f7ee3e8ba44ed155c1fe18901f00d79d
Daiki Hirabayashi <hirabayashi@infoscience.co.jp>
2020-10-26 22:36:40 +0900

    ストライクの場合、スペアの場合のスコア計算処理の追加

--- js/score.js
+++ js/score.js
@@ -32,6 +32,18 @@ new Vue({
     },
     methods: {
         changeScore(frameIndex, selectIndex, event) {
+            function canDetermineScoreAtPrevious(_this) {
+                // この関数の中でthisを直接使うと想定と違うオブジェクトを指してしまうため、引数で受け取る
+                return frameIndex >= 1 && selectIndex === 1
+                    && _this.frames[frameIndex-1].scores[0].getStatus() === STRIKE_OCCURRED;
+            }
+
+            function canDetermineScoreAtTwoPrevious(_this) {
+                return frameIndex >= 2 && selectIndex == 0
+                    && _this.frames[frameIndex-2].scores[0].getStatus() === STRIKE_OCCURRED
+                    && _this.frames[frameIndex-1].scores[0].getStatus() === STRIKE_OCCURRED;
+            }
+
             let options = ['', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
             let value = event.target.value;
             let intValue = 0;
@@ -46,48 +58,52 @@ new Vue({
             // ストライク
             if(selectIndex === 0 && intValue === 10) {
                 score.informStrikeOccurred();
-                return;
             // スペア
             } else if(selectIndex === 1
                     && (this.frames[frameIndex].scores[selectIndex-1].intValue() + score.intValue() === 10)) {
                 score.informSpareOccurred();
-                return;
-            }
-
-            score.informScoreDetermined();
-
-            // 一投目のプルダウンで値が選択された場合、値に応じて二投目のプルダウンの状態を変える
-            if(selectIndex === 0) {
-                if(value === '') {
-                    this.frames[frameIndex].selectBoxes[selectIndex+1].disabled = true;
-                    this.frames[frameIndex].selectBoxes[selectIndex+1].options = options
-                } else {
-                    this.frames[frameIndex].selectBoxes[selectIndex+1].disabled = false;
-                    // 合計スコアが10を超えないように二投目のプルダウンで選択可能な値を絞る
-                    let trancated_options = [];
-                    let length = options.length - value - 1;
-                    for(let i = 0; i < length; i++) {
-                        trancated_options.push(i);
+            } else {
+                score.informScoreDetermined();
+                // 一投目のプルダウンで値が選択された場合、値に応じて二投目のプルダウンの状態を変える
+                if(selectIndex === 0) {
+                    if(value === '') {
+                        this.frames[frameIndex].selectBoxes[selectIndex+1].disabled = true;
+                        this.frames[frameIndex].selectBoxes[selectIndex+1].options = options
+                    } else {
+                        this.frames[frameIndex].selectBoxes[selectIndex+1].disabled = false;
+                        // 合計スコアが10を超えないように二投目のプルダウンで選択可能な値を絞る
+                        let trancated_options = [];
+                        let length = options.length - value - 1;
+                        for(let i = 0; i < length; i++) {
+                            trancated_options.push(i);
+                        }
+                        this.frames[frameIndex].selectBoxes[selectIndex+1].options = trancated_options;
                     }
-                    this.frames[frameIndex].selectBoxes[selectIndex+1].options = trancated_options;
                 }
             }
 
-
             // 2フレーム以降は、前にストライクやスペアが起きていてスコア未確定の可能性がある
             // TODO 10フレーム目の対応
-            // ストライクの場合
-            // TODO ストライクが連鎖する場合はうまく動かない
-            if(frameIndex >= 1 && selectIndex === 1
-                    && this.frames[frameIndex-1].scores[0].getStatus() === STRIKE_OCCURRED) {
-                
+            // 一つ前のフレームのストライクのスコアを決定できる場合
+            if(canDetermineScoreAtPrevious(this)) {                
                 this.frames[frameIndex-1].scores[0].add(this.frames[frameIndex].scores[0].intValue() + intValue);
                 this.frames[frameIndex-1].scores[0].informScoreDetermined();
                 this.frames[frameIndex-1].scores[1].informScoreDetermined();                
             }
 
-            // TODO スペアの場合
-            
+            // 二つ前のフレームのストライクのスコアを決定できる場合（ストライクが続いた場合に発生）
+            if(canDetermineScoreAtTwoPrevious(this)) {                
+                this.frames[frameIndex-2].scores[0].add(this.frames[frameIndex-1].scores[0].intValue() + intValue);
+                this.frames[frameIndex-2].scores[0].informScoreDetermined();
+                this.frames[frameIndex-2].scores[1].informScoreDetermined();                
+            }
+
+            // 一つ前のスペアのスコアを決定できる場合
+            if(frameIndex >= 1 && selectIndex === 0
+                    && this.frames[frameIndex-1].scores[1].getStatus() === SPARE_OCCURRED) {
+                this.frames[frameIndex-1].scores[1].add(intValue);
+                this.frames[frameIndex-1].scores[1].informScoreDetermined();                
+            }
         
             // 完全には入力されていない場合は何も表示しない
             let scores = this.frames[frameIndex].scores.filter(function(score) {
@@ -96,7 +112,6 @@ new Vue({
             if(scores.length !== this.frames[frameIndex].scores.length) {
                 return;
             }
-
         },
         calcTotalScore(frameIndex) {
             let totalScore = 0;
